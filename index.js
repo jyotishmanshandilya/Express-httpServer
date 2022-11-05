9b2a46abbcce2969a04f07cc549a7a4d15524a99
jyotishman <jyotishmanshandilyaggn@gmail.com>
2022-11-05 15:43:47 +0530

    Express Simple REST

--- index.js
+++ index.js
@@ -2,12 +2,57 @@ const express = require('express');
 const res = require('express/lib/response');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes', (req,res,next)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
+
+app.get('/dishes', (req,res,next)=>{
+    res.end('Will send all the dishes');
+});
+
+app.post('/dishes', (req,res,next)=>{
+    res.end("Will add the dish: "+req.body.name+" with details: "+req.body.description)
+});
+
+app.put('/dishes', (req,res,next)=>{
+    res.statusCode = 403;
+    res.end("PUT request not supported");
+});
+
+app.delete('/dishes', (req,res,next)=>{
+    res.end('Deleting all the dishes');
+});
+
+
+app.get('/dishes/:dishId', (req,res,next)=>{
+    res.end('Will send details of the dish: '+req.params.dishId+' to you');
+});
+
+app.post('/dishes/:dishId', (req,res,next)=>{
+    res.statusCode = 403;
+    res.end("POST request not supported on /dish/"+req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req,res,next)=>{
+    res.write("Updating the dish "+req.params.dishId);
+    res.end("Will update the dish: "+ req.body.name 
+            + " with the details: "+req.body.description);
+});
+
+app.delete('/dishes/:dishId', (req,res,next)=>{
+    res.end('Deleting the dish: '+req.params.dishId);
+});
 
 app.use(express.static(__dirname + '/public'));
 
@@ -15,7 +60,7 @@ app.use((req, res, next)=>{
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-})
+});
 
 const server = http.createServer(app);
 server.listen(port, hostname, ()=>{
